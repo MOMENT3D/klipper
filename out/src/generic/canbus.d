ae2b97b9c40d13a994586f55f5527e455f665014
SHID1214 <sanghun.id@gmail.com>
2024-02-21 14:25:10 +0900

    Edit /klipper/out

---
+++ out/src/generic/canbus.d
@@ -0,0 +1,6 @@
+out/src/generic/canbus.o: src/generic/canbus.c out/autoconf.h \
+ src/generic/canbus.h /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h \
+ src/generic/canserial.h src/command.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdarg.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stddef.h src/ctr.h \
+ src/compiler.h
---
+++ out/src/stm32/fdcan.d
@@ -0,0 +1,11 @@
+out/src/stm32/fdcan.o: src/stm32/fdcan.c src/command.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdarg.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stddef.h \
+ /usr/lib/gcc/arm-none-eabi/8.3.1/include/stdint.h src/ctr.h \
+ src/compiler.h src/generic/armcm_boot.h src/ctr.h src/generic/canbus.h \
+ src/generic/canserial.h src/stm32/internal.h out/autoconf.h \
+ lib/stm32g0/include/stm32g0xx.h lib/stm32g0/include/stm32g0b1xx.h \
+ lib/cmsis-core/core_cm0plus.h lib/cmsis-core/cmsis_version.h \
+ lib/cmsis-core/cmsis_compiler.h lib/cmsis-core/cmsis_gcc.h \
+ lib/cmsis-core/mpu_armv7.h lib/stm32g0/include/system_stm32g0xx.h \
+ src/sched.h
